c5aae8bdc7174e71ec310b4b22a5b1d345e51943
Smear Lab <nhess@uoregon.edu>
2026-03-28 16:26:50 -0700

    morris dry maze

---
+++ PythonTransform/scratch.py
@@ -0,0 +1,19 @@
+# Monitor flipping behavior disabled for Dry-MM task
+
+@returns(float)
+def process(value):
+    # time_period = value.Item1
+    # session_num = int(value.Item2[1:])
+
+    # if session_num % 2 == 0:
+    #   if time_period == 0:
+    #     return 1.5
+    #   else:
+    #     return -1.5
+
+    # else:
+    #   if time_period == 0:
+    #     return -1.5
+    #   else:
+    #     return 1.5
+    return 1.5
