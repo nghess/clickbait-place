87a191fa4ce988e9630fde2b4077f1118c3476c5
Smear Lab <nhess@uoregon.edu>
2026-05-07 17:54:53 -0700

    cbm odor pid tested

---
+++ clickbait-motivate-odor/air_logic.py
@@ -0,0 +1,34 @@
+@returns(tuple)
+def process(value):
+    flip_state = value.Item1
+    reward_state = value.Item2[2]
+    active_target = value.Item2[3]
+
+    left_air = False
+    right_air = False
+    fans = 0
+
+    if active_target is not None and reward_state == False:
+        if flip_state == 0:
+            left_air = True
+            right_air = False
+            fans = 0
+        elif flip_state == 1:
+            left_air = False
+            right_air = True
+            fans = 0
+        elif flip_state == 2:
+            left_air = False
+            right_air = False
+            fans = 255
+
+    elif reward_state == True:
+        left_air = False
+        right_air = False
+        fans = 255
+    else:
+        left_air = False
+        right_air = False
+        fans = 0
+
+    return (left_air, right_air, fans)
